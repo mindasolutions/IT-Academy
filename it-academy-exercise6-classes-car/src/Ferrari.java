7fbda649b7468f4d91cca233bf24b94e545369d1
Dawid Minda <dawid@minda.pl>
2019-03-17 20:00:41 +0100

    Add overloaded method, fix code.

--- it-academy-exercise6-classes-car/src/Ferrari.java
+++ it-academy-exercise6-classes-car/src/Ferrari.java
@@ -2,11 +2,13 @@ public class Ferrari {
     public final String fuel;
     public static int numberOfSeats;
     private int mileage;
+    private String unit;
 
     public Ferrari(String fuel, int numberOfSeats, int mileage) {
         this.fuel = fuel;
         this.numberOfSeats = numberOfSeats;
         this.mileage = mileage;
+        this.unit = "km";
     }
 
     public String getFuel() {
@@ -21,11 +23,23 @@ public class Ferrari {
         return mileage;
     }
 
+    public String getUnit() {
+        return unit;
+    }
+
     public static void setNumberOfSeats(int numberOfSeats) {
         Ferrari.numberOfSeats = numberOfSeats;
     }
 
     public void setMileage(int mileage) {
         this.mileage = mileage;
+        this.unit = "mil";
     }
+
+    public void setMileage(int mileage, String unit) {
+        this.mileage = mileage;
+        this.unit = unit;
+    }
+
+    //public String setFuel(String typeOfFuel) { this.fuel = typeOfFuel}; - setter cannot be created for final var.
 }
--- it-academy-exercise6-classes-car/src/MainClass.java
+++ it-academy-exercise6-classes-car/src/MainClass.java
@@ -5,26 +5,62 @@ public class MainClass {
 
         System.out.println("--- myFerrari ---");
         System.out.println("Fuel type: " + myFerrari.getFuel());
-        System.out.println("Current mileage: " + myFerrari.getMileage());
+        System.out.println("Current mileage: " + myFerrari.getMileage() + myFerrari.getUnit());
+        myFerrari.setMileage(20000);
+        System.out.println("Current mileage: " + myFerrari.getMileage() + myFerrari.getUnit());
+        myFerrari.setMileage(22000, "km");
+        System.out.println("Current mileage: " + myFerrari.getMileage() + myFerrari.getUnit());
         System.out.println("Number of seats: " + myFerrari.getNumberOfSeats());
         System.out.println();
+
+        /*
+        Output:
+            --- myFerrari ---
+            Fuel type: ON
+            Current mileage: 13000km
+            Current mileage: 20000mil
+            Current mileage: 22000km
+            Number of seats: 2
+         */
 
         Ferrari myFerrari2 = new Ferrari("95", 4, 11000);
 
         System.out.println("--- myFerrari2 ---");
         System.out.println("Fuel type: " + myFerrari2.getFuel());
-        System.out.println("Current mileage: " + myFerrari2.getMileage());
+        System.out.println("Current mileage: " + myFerrari2.getMileage() + myFerrari.getUnit());
         System.out.println("Number of seats: " + myFerrari2.getNumberOfSeats());
         System.out.println();
+
+        /*
+        Output:
+            --- myFerrari2 ---
+            Fuel type: 95
+            Current mileage: 11000km
+            Number of seats: 4
+         */
 
         System.out.println("After changing numberOfSeats, myFerrari shows: " + myFerrari.getNumberOfSeats());
         System.out.println("Now i change Ferrari.numberOfSeats to 3");
         myFerrari.numberOfSeats = 3;
         System.out.println();
 
+        /*
+        Output:
+            After changing numberOfSeats, myFerrari shows: 4
+            Now i change Ferrari.numberOfSeats to 3
+         */
+
         System.out.println("myFerrari shows: " + myFerrari.getNumberOfSeats());
         System.out.println("myFerrari2 shows: " + myFerrari.getNumberOfSeats());
         // myFerrari.mileage = 30000; - cannot, private var
         // myFerrari.fuel = "Diesel"; - cannot, final var
+
+        /*
+        Output:
+            myFerrari shows: 3
+            myFerrari2 shows: 3
+         */
+
+
     }
 }
